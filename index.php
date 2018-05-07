225ac3d7f9d7dfd855a12e2539ade603a2a0494f
Max <max16120900@gmail.com>
2018-05-07 09:46:42 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Warden Potato
+ * Date: 5/7/2018
+ * Time: 09:31
+ */
+
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+
+echo"$a. Ik heb alleen $c manieren gevonden $b";
+echo"<br>";
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+echo"Mens" . "$b $a" . $b ." mak" . $b . ", werk" . "$b $c";
+echo"<br>";
+
+$a = "ry";
+$b = ", but";
+$c = "fail";
+echo"T" . $a . " and $c" . $b . " never $c to t$a.";
+echo"<br>";
+
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+echo"De cursus $a $b $c" . "rstellingen\" kan vanavond helaas niet doorgaan.";
